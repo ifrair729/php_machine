7c0c7f63ee2c050c827d82805492d7974562c2e8
ifrair729 <142362748+ifrair729@users.noreply.github.com>
2025-01-10 00:28:46 +0000

    end of jan 9

---
+++ php_practice/hello.php
@@ -0,0 +1,8 @@
+<?php
+
+$myName = "Isaac";
+$myFriend = "Levi";
+echo "Hello, World!\n";
+echo "My name is $myName\n";
+echo "My friend's name is $myFriend\n";
+echo 'Hello $myFriend.' . ' What is happening, bro?';
